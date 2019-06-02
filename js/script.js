821b022ff14ae7422fac4bf9aaacfd780bc99e97
bridget <breekiki03@gmail.com>
2019-06-02 23:11:01 +0300

    Add variables in the js file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function findout() {
+var lineA = parseInt(document.getElementById("sideA").value);
+var lineB = parseInt(document.getElementById("sideB").value);
+var lineC = parseInt(document.getElementById("sideC").value);
+}
