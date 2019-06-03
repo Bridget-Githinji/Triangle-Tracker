6faeba6867ba8d67bd06ed8bfcbbf070ace338b1
bridget <breekiki03@gmail.com>
2019-06-03 04:30:11 +0300

    Add the script tags on the html structure

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
 function findout() {
-var valA = parseInt(document.getElementById("sideA").value);
-var valB = parseInt(document.getElementById("sideB").value);
-var valC = parseInt(document.getElementById("sideC").value);
-
-if (valA === valB === valC){
-    alert("This is an equilateral triangle");
-}
-
-}
+    var val1=document.getElementById("sideA").value;
+    var val2=document.getElementById("sideB").value;
+    var val3=document.getElementById("sideC").value;
+    var sideA=parseInt(inp1);
+    var sideB=parseInt(inp2);
+    var sideC=parseInt(inp3);
+    
+    if (sideA === sideB === sideC){
+      alert("EQUILATERAL TRAIANGLE");
+    
