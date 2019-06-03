3b0557810c8f88336f5923c5e2f60c395b41be64
bridget <breekiki03@gmail.com>
2019-06-03 05:15:08 +0300

    Add the third conditional statement

---
+++ script.js
@@ -0,0 +1,16 @@
+function findout() {
+    var val1 = parseInt(document.getElementById("sideA").value);
+    var val2 = parseInt(document.getElementById("sideB").value);
+    var val3 = parseInt(document.getElementById("sideC").value);
+
+    if(val1 === val2 && val2 === val3 && val3 === val1) {
+        alert("This is an Equilateral Triangle");
+    }
+    else if(val1 === val2 && val1 !== val3) {
+        alert("This is an Isosceles Triangle");
+    }
+    else if(val1 === val3 && val1 !== val2) {
+        alert("This is an Isosceles Triangle");
+    }
+    
+}
